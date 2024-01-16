affd2481e2e759b7b4e2648759c1651acba730f6
oczonZ <156734288+oczonZe@users.noreply.github.com>
2024-01-16 13:43:58 +0800

    Update Program.cs

--- CSharpRampup/Program.cs
+++ CSharpRampup/Program.cs
@@ -21,6 +21,26 @@
         double temperatureCel = (temperatureFah - 32) * 5.0 / 9.0;
         Console.WriteLine("Temperature in Celsius (C):" + temperatureCel);
         Console.WriteLine("==================================================");
+
+        int[] studentAges = new int[10];
+        int totalAge = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Enter age of student " + (i + 1) + ": ");
+            studentAges[i] = Convert.ToInt32(Console.ReadLine());
+            totalAge += studentAges[i];
+        }
+
+        double averageAge = totalAge / 10.0;
+
+        // Outputting ages and average age
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Age of Student " + (i + 1) + ": " + studentAges[i]);
+        }
+
+        Console.WriteLine("The average age of the students is: " + averageAge.ToString("F2"));
         
         }
     }
