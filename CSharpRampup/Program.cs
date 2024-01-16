d6daf5547b2c2458edd1b1a74a3c8a26c889a5db
oczonZ <156734288+oczonZe@users.noreply.github.com>
2024-01-16 12:22:49 +0800

    Update Program.cs
    added rampup part 1

--- CSharpRampup/Program.cs
+++ CSharpRampup/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        Console.WriteLine("Weight in Pounds (lbs):");
+        double pounds = Convert.ToDouble(Console.ReadLine());
+        double weightKg = pounds * 0.453592;
+        Console.WriteLine("Weight converted to Kilograms (kg):" + weightKg);
+        Console.WriteLine("==================================================");
+        
+        Console.WriteLine("Length in Miles (mi):");
+        double lengthMi = Convert.ToDouble(Console.ReadLine());
+        double lengthKm = lengthMi * 1.60934;
+        Console.WriteLine("Length in Kilometers (km):" + lengthKm);
+        Console.WriteLine("==================================================");
+        
+        Console.WriteLine("Temperature in Fahrenheit (F):");
+        double temperatureFah = Convert.ToDouble(Console.ReadLine());
+        double temperatureCel = (temperatureFah - 32) * 5.0 / 9.0;
+        Console.WriteLine("Temperature in Celsius (C):" + temperatureCel);
+        Console.WriteLine("==================================================");
+        
         }
     }
 }
